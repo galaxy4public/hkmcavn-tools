eb8b36d1111700ddd22d9ed672d5103d61d8f864
(GalaxyMaster) <galaxy4public@users.noreply.github.com>
2022-12-15 21:01:27 +1100

    - Initial commit of xcrypt.py

---
+++ xcrypt/README.md
@@ -0,0 +1,97 @@
+xcrypt.py
+===
+
+This is a small (and not very Pythonic) Python script that implements both
+decryption and encryption of the firmware files as distributed by several
+automotive companies who are using the head unit solution designed by LG
+Electronics (LGe).  These head units can be found in modern KIAs, Hyindais,
+GMs, and Hondas (according to LGe's B2B site).  However, the work and all the
+tests are performed on using firmware related to 2022 KIA Sportage GT-Line as
+it was distributed in Australia.
+
+Usage
+---
+
+The script currently supports three modes of operation:
+
+1. testing whether a file (or a group of files) are encrypted or not encrypted
+
+   The following session excerpt is from running the script aagainst all files in
+   the directory with downloaded firmware for 2022 KIA Sportage:
+
+       $ ~/hkmcavn-tools/xcrypt/xcrypt.py -t -v $(find . -type f)
+       no output was specified, will prepend ".out" suffix to processed files
+       Performing a determination whether files are encrypted or not
+       ./2022_Sportage_AU.ver: metadata signature was not found
+       ./2022_Sportage_AU.ver => not encrypted or invalid
+       ./whatsnew/whatsnew/KIA/RU/update-ko_KR.json => encrypted
+       ./whatsnew/whatsnew/KIA/RU/whatsnew-el_GR.url => encrypted
+
+   This mode of operation is a side-effect of the decryption routines, hence
+   you may see a strange message in regard to the output prefix when the mode does
+   not modify anything.  However, this mode might be useful to quickly assess what
+   is encrypted in the directory tree and what is not, since Kia/Hyindai tend to
+   provide both encrypted and plain firmware files inside their firmware updates.
+
+   The command above was running with the verbose flag specified.  If the flag is
+   omitted the script would not produce any output, but will indicate through the
+   exit code whether it encountered any unencrypted files (exit code > 0) and one
+   can determine what kind of an issue the script has encountered by checking the
+   exit code (please see the script source to understand what different exit codes
+   mean).  For a very simple use case, one can just run `xcrypt.py -t file_to_check`
+   and if exit code is zero, then the file is encrypted and most likely can be
+   decrypted using the scriptr. If the exit code is not zero, then the file is
+   either not encrypted or damaged.
+
+2. decrypting one or more files
+
+   The following demonstrates how to decrypt a single file:
+
+       $ ~/hkmcavn-tools/xcrypt/xcrypt.py -d -v ~/KIA/2022_Sportage_AU/.lge.upgrade.xml -o .lge.upgrade.xml
+       Performing a decryption
+       /home/galaxy/KIA/2022_Sportage_AU/.lge.upgrade.xml => .lge.upgrade.xml
+
+
+3. encrypting one or more files
+
+   Below is an example of a session where a plain-text version of the XML
+   file is encrypted back to its version from the firmware distribution:
+
+       $ ~/hkmcavn-tools/xcrypt/xcrypt.py -e -v .lge.upgrade.xml -o encrypted/.lge.upgrade.xml
+       Performing an encryption
+       .lge.upgrade.xml => encrypted/.lge.upgrade.xml
+
+   Note: the encryption logic implemented by LGe relies on the file name, so
+   if you rename an encrypted file it would be possible to decrypt it using
+   the standard logic (perhaps a new mode needs to be introduced to specify
+   the original name of the file for these cases?).
+
+You can always get a help output with `-h` if you are not sure which options to
+use, but since the script is in its early development there is no point of
+documenting the options here at the moment.
+
+When multiple files are specified on the command line for either encryption or
+decryption, the only accepted value for the output is a directory.  As it is
+currently implemented, the script will create full paths to files it was
+working on under the specified directory, e.g. if you ask to decrypt files
+`../abc.def` and `/somewhere/fed.bca` and put output into `out/`, then the
+script will create `out/home/user/abc.def` (if that relative path to `abc.def`
+was pointing to a file in user's home directory) and `out/somewhere/fed.bca`.
+Most likely, this behaviour will be tuned in future versions of the script, but
+right now it works as described above.
+
+Thanks
+---
+
+The creation of this script would not be possible without generous help from
+the community at [Gen5 Wide-Open](https://g4933.gitlab.io/wideopen/) at their
+Telegram channel.  Specifically, the following people contributed the most
+(listed in no particular order):
+
+  - WideOpen (thanks for sharing previous research on the topic)
+  - Brody (thanks for prompt responses and bootstrapping thisresearch)
+  - Alexey Murzilin (thanks for willingness to sacrifice your time for running commands on the real hardware)
+  - AlexE (thanks for providing data from the real hardware)
+  - Rob (thanks for keeping me up to date with news and moral support)
+
+
---
+++ xcrypt/xcrypt.py
@@ -0,0 +1,260 @@
+#!/bin/env python
+import argparse
+import os
+import sys
+import time
+
+from base64 import b64encode
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+
+print_v = print
+KEY = '60a93d70f73302a63e5ed0d0ea38be22'
+IV = ''
+META_SIZE=0x4024    # size of the metadata blob
+PADDING=b''
+
+def calculate_iv(file, size):
+    basename = os.path.basename(file)
+    check_code = SHA256.new()
+    check_code.update(f'{os.path.basename(file)}{size}'.encode('utf-8'))
+    return check_code.hexdigest()[:32]
+
+def perform_test(file):
+    global IV
+    global PADDING
+    result = 0xFF
+
+    with open(file, 'rb') as f:
+        f.seek(0, os.SEEK_END)
+        file_size = f.tell()
+        if file_size <= META_SIZE:
+            print_v(f'{file}: file is too small to be encrypted')
+            return 0x01
+
+        f.seek(file_size - META_SIZE, os.SEEK_SET)
+        metadata = f.read(META_SIZE)
+        if not metadata:
+            print_v(f'{file}: failed to read metadata from the file')
+            return 0x20
+
+    if metadata[:2] != b'TE':
+        print_v(f'{file}: metadata signature was not found')
+        return 0x2
+
+    if not metadata[2] in [ 50, 82 ]:
+        print_v(f'{file}: unknown version of metadata')
+        return 0x4
+
+    if metadata[2] == 82:
+        print_v(f'{file}: unsupported version of metadata')
+        return 0x8
+
+    IV = calculate_iv(file, file_size - META_SIZE)
+
+    check_code = SHA256.new()
+    check_code.update(bytes.fromhex(IV))
+    check_code.update(metadata[36:])
+    if metadata[4:36] != check_code.digest():
+        print_v(f'{file}: metadata integrity check failed')
+        return 0x10
+
+    PADDING=metadata[36:52]             # let's save embedded padding vector
+
+    result = {}
+    result['file'] = file
+    result['size'] = file_size - META_SIZE
+    result['padding'] = metadata[36:52]
+    result['iv'] = IV
+    return result
+
+def perform_encrypt(block, cipher, check_code):
+    BLOC=16
+    index = 0
+    encd = b''
+
+    pad = 0
+    if (len(block) % BLOC) > 0:
+        pad = BLOC - len(block) % BLOC
+        block += b'\0' * pad
+
+    while index < len(block):
+        encd += cipher.encrypt(block[index:index+BLOC])
+        index += BLOC
+
+    if pad:
+        # in out use case this branch can only happen at the end of the file
+        # encryption routine, so we are abusing it to generate the metadata.
+        metadata = bytearray(META_SIZE-1) # META_SIZE
+        metadata[0:2] = b'TE2'
+        metadata[36:36+pad] = encd[-pad:]
+        check_code.update(metadata[36:])
+        metadata[4:36] = check_code.digest()
+        return encd[:len(block) - pad] + metadata
+
+    return encd
+
+
+def perform_decrypt(block, cipher, param):
+    BLOC=16
+    index = 0
+    decd = b''
+
+    #print(f' {len(block)}:{len(block) % BLOC}')
+    pad = 0
+    if (len(block) % BLOC) > 0:
+        #print(f'short block: {len(block)} by {len(block)%BLOC}')
+        pad = BLOC - len(block) % BLOC
+        #block += bytes.fromhex('83d1d2324f8a69f889f8')
+        block += PADDING[:pad]
+        #print(f'padded by {pad} bytes to total {len(block)} ({PADDING[:pad].hex()})')
+
+    while index < len(block):
+        decd += cipher.decrypt(block[index:index+BLOC])
+        index += BLOC
+    return decd[:len(block) - pad]
+
+
+def read_in_chunks(file_object, chunk_size=4*1024, limit = -1):
+    """Lazy function (generator) to read a file piece by piece.
+    Default chunk size: 1MB."""
+    ptr = 0
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            #print(f'end of file, read {ptr} chunks of {chunk_size} with limit set to {limit}')
+            break
+        ptr = ptr + 1
+        if limit > 0 and len(data) > limit - (ptr-1)*chunk_size:
+            #print_v('processing truncated block')
+            #print(f'read {ptr-1} chunks of {chunk_size} + {len(data)} with limit set to {limit}')
+            #print(f'giving out {limit - (ptr-1)*chunk_size}')
+            yield data[:limit - (ptr-1)*chunk_size]
+            break
+        yield data
+
+
+def process_block(mode, block, cipher, param):
+    return mode(block, cipher, param)
+
+def update_progress(file, size, block):
+    count = 0
+    while size > count * block:
+        count += 1
+        percent = (count * block * 100) / size
+        # out block is bigger than actual data read
+        percent = min(percent, 100.0)
+        print_v(f'\r{file} .. {percent:3.0f}%', end='')
+        yield percent
+
+def process_file(mode, file, block_size, output):
+    #file_size = os.path.getsize(file)
+
+    if mode == perform_decrypt:
+        metadata = perform_test(file)   # this extracts and populates IV
+        if isinstance(metadata, int) and metadata != 0:
+            print(f'\r{file} is either unencrypted or damaged (use -v -t to see the details)')
+            return
+        file_size = metadata['size']
+        iv = bytes.fromhex(metadata['iv'])
+        param = ''
+    elif mode == perform_encrypt:
+        file_size = os.path.getsize(file)
+        iv = bytes.fromhex(calculate_iv(file, file_size))
+        param = SHA256.new()
+        param.update(iv)
+
+    with open(file, 'rb') as file_in:
+        with open(output, 'xb') as file_out:
+            cipher = AES.new(bytes.fromhex(KEY), AES.MODE_CBC, iv)
+            print_v('\r\033[K', end='')
+            progress = update_progress(file, file_size, block_size)
+            for block in read_in_chunks(file_in, block_size, file_size):
+                file_out.write(process_block(mode, block, cipher, param))
+                percent = next(progress)
+            print_v('\r', end='')
+
+
+def main():
+    global print_v
+
+    parser = argparse.ArgumentParser()
+    mode = parser.add_mutually_exclusive_group()
+    mode.add_argument('-t', '--test', action='store_true',
+                        help='test for encryption signature')
+    mode.add_argument('-e', '--encrypt', action='store_true',
+                        help='perform encryption')
+    mode.add_argument('-d', '--decrypt', action='store_true',
+                        help='perform encryption')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='enable verbose output')
+    parser.add_argument('-b', '--block-size', type=int, dest='block_size',
+                        help='sets the read buffer in bytes (default 4096)',
+                        default=4*1024)
+    parser.add_argument('-o', '--output', type=str, dest='output',
+                        help="output directory where result will be stored")
+    parser.add_argument('file', type=str, nargs='+',
+                        help="a list of input files for the selected operation")
+    args = parser.parse_args()
+
+    print_v = print if args.verbose else lambda *a, **k: None
+
+    if args.block_size % 16:
+        print('ERROR: the specified block size is not aligned, should be dividable by 16!')
+        sys.exit(1)
+
+    if args.output:
+        if args.output[-1] == os.sep:
+            print_v('output set to a directory')
+            if not os.path.isdir(args.output):
+                print_v(f'creating output directory "{args.output}"')
+                os.makedirs(args.output)
+        else:
+            if len(args.file) > 1:
+                print(f'ERROR: cannot process multiple files into one, check that "-o" argument ends with "{os.sep}"')
+                sys.exit(1)
+    else:
+        print_v('no output was specified, will prepend ".out" suffix to processed files')
+
+    # let's abuse the mode variable :)
+    print_v('Performing ', end='')
+    if args.test:
+        print_v('a determination whether files are encrypted or not')
+        mode = perform_test
+    elif args.encrypt:
+        print_v('an encryption')
+        mode = perform_encrypt
+    else:
+        print_v('a decryption')
+        mode = perform_decrypt
+
+    result = 0
+    for file in args.file:
+        if not os.access(file, os.R_OK):
+            print_v(f'{file} does not exist or is not readable, skipping')
+            continue
+        if args.test:
+            file_result = perform_test(file)
+            if isinstance(file_result, int) and file_result != 0:
+                result |= file_result
+                print_v(f'{file} => not encrypted or invalid')
+            else:
+                print_v(f'{file} => encrypted')
+        else:
+            output_file = args.output
+            if args.output:
+                if args.output[-1] == os.sep:
+                    output_file = os.path.join(args.output, os.path.abspath(file)[1:])
+                    print(f'{output_file}')
+                    if not os.path.isdir(os.path.dirname(output_file)):
+                        print_v(f'creating output directory "{args.output}"')
+                        os.makedirs(os.path.dirname(output_file))
+            else:
+                output_file = f'{file}.out'
+            process_file(mode, file, args.block_size, output_file)
+            print_v(f'{file} => {output_file}')
+
+    sys.exit(result)
+
+if __name__ == '__main__':
+    main()
